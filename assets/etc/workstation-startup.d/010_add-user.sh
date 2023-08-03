9b4840cf7cae769bdd4d6d5c8d088cdb7beb04a6
maucaro <maucaro@outlook.com>
2023-08-03 15:57:51 -0400

    Adding proxy environmt variables

--- assets/etc/workstation-startup.d/010_add-user.sh
+++ assets/etc/workstation-startup.d/010_add-user.sh
@@ -14,4 +14,10 @@ passwd -d user >/dev/null
 echo "%sudo ALL=NOPASSWD: ALL" >> /etc/sudoers
 adduser xrdp ssl-cert  
 
+# Set proxy environment variables
+file=/home/user/.bashrc
+[ ! -z "${http_proxy}" ] && echo "http_proxy=$http_proxy" >> $file
+[ ! -z "${https_proxy}" ] && echo "https_proxy=$https_proxy" >> $file
+[ ! -z "${no_proxy}" ] && echo "no_proxy=$no_proxy" >> $file
+
 echo "Exiting 010_add-user.sh"
