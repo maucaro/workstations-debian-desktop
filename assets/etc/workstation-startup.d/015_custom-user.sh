611b3c3a12254eb2fc45a3b846e2c625c4ae936a
maucaro <maucaro@outlook.com>
2023-08-16 06:27:07 -0400

    Proxy settings changed to /etc/profile.d

--- assets/etc/workstation-startup.d/015_custom-user.sh
+++ assets/etc/workstation-startup.d/015_custom-user.sh
@@ -9,9 +9,11 @@ echo "Starting 015_custom-user.sh"
 set -e
 
 # Set proxy environment variables per values passed in to the container (--env) if present
-file=/home/user/.bashrc
-[ ! -z "${http_proxy}" ] && echo "http_proxy=$http_proxy" >> $file
-[ ! -z "${https_proxy}" ] && echo "https_proxy=$https_proxy" >> $file
-[ ! -z "${no_proxy}" ] && echo "no_proxy=$no_proxy" >> $file
+file=/etc/profile.d/proxy.sh
+[ ! -z "${http_proxy}" ] && echo "export http_proxy=$http_proxy" >> $file
+[ ! -z "${https_proxy}" ] && echo "export https_proxy=$https_proxy" >> $file
+[ ! -z "${no_proxy}" ] && echo "export no_proxy=$no_proxy" >> $file
+
+chmod +x $file
 
 echo "Exiting 015_custom-user.sh"
